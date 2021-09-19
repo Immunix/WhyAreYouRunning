22910d8a2cb72d98f80ed808a6948a7a1d61b556
Immunix <mirceagighileanu@yahoo.com>
2021-09-19 23:09:44 +0200

    project dependencies

---
+++ app/src/main/java/com/immunix/whyareyourunning/Application.kt
@@ -0,0 +1,21 @@
+package com.immunix.whyareyourunning
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.util.CoilUtils
+import okhttp3.OkHttpClient
+
+class Application : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(applicationContext)
+            .crossfade(true)
+            .okHttpClient {
+                OkHttpClient.Builder()
+                    .cache(CoilUtils.createDefaultCache(applicationContext))
+                    .build()
+            }
+            .build()
+    }
+}
